0b3bb1e234f8524260a4e4ebed73db6fa6267131
robl <robl@monkeyhelper.com>
2010-12-17 23:21:43 +0000

    Added License info for ExtractContent

--- lib/pismo/reader/cluster.rb
+++ lib/pismo/reader/cluster.rb
@@ -1,10 +1,30 @@
-require 'nokogiri'
-require 'sanitize'
-begin; require 'ap'; rescue LoadError; end
-
 module Pismo
   module Reader
     class Cluster < Base
+
+      # Adapted from : http://rubyforge.org/projects/extractcontent/
+      #
+      # Portions of this code are :
+      # Copyright (c) 2007/2008 Nakatani Shuyo / Cybozu Labs Inc. All rights reserved.
+      #
+      # Permission is hereby granted, free of charge, to any person obtaining
+      # a copy of this software and associated documentation files (the
+      # "Software"), to deal in the Software without restriction, including
+      # without limitation the rights to use, copy, modify, merge, publish,
+      # distribute, sublicense, and/or sell copies of the Software, and to
+      # permit persons to whom the Software is furnished to do so, subject to
+      # the following conditions:
+      #
+      # The above copyright notice and this permission notice shall be
+      # included in all copies or substantial portions of the Software.
+      #
+      # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+      # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+      # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+      # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+      # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+      # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+      # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
       # Default option parameters
       DEFAULTS = {
