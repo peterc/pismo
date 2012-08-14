d2fc339c03fce79efa74820db3977aab39d81a12
Adam Crownoble <adam.crownoble@biola.edu>
2012-08-14 15:15:47 -0700

    Set explicit timezone during testing.
    The metadata_expected.yaml includes datetimes with a +01:00 time zone. Which
    means the test will fail for anybody running the test in a different time zone.
    Explicitly setting the time zone to UTC in the helper solves this problem.

--- test/helper.rb
+++ test/helper.rb
@@ -9,6 +9,9 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'pismo'
 
+# Set time zone to prevent time interpretations not matching thoes stored in metadata_expected.yaml
+ENV['TZ'] = 'UTC'
+
 class Test::Unit::TestCase
   include Pismo
   HTML_DIRECTORY = File.dirname(__FILE__) + "/corpus"
